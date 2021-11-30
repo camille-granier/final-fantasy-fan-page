adcb0918f5eefb13ea1238d72bfba9c93214d33f
camille-granier <granierc@protonmail.com>
2021-11-30 20:01:02 +0100

    loading spinner for Creature component

--- src/pages/Creatures.js
+++ src/pages/Creatures.js
@@ -3,17 +3,20 @@ import Navigation from '../components/Navigation';
 import axios from 'axios';
 import CardC from '../components/CardC';
 import Logo from '../components/Logo';
+import LoadingSpinner from '../UI/Loadingspinner';
+
 const Creatures = () => {
   const [data, setData] = useState([]);
   // eslint-disable-next-line
   const [playOnce, setPlayOnce] = useState(true);
   // eslint-disable-next-line
   const [selectedGame, setSelectedGame] = useState('');
-  const games = ["All Games", "Final Fantasy 01", "Final Fantasy BE", "Final Fantasy 02", "Final Fantasy 03", "Final Fantasy 04", "Final Fantasy 05", "Final Fantasy 06", "Final Fantasy 07", "Final Fantasy 08", "Final Fantasy 09", "Final Fantasy 10", "Final Fantasy 10-2", "Final Fantasy 12", "Final Fantasy 13", "Final Fantasy 13-2", "Final Fantasy 15"];
   // eslint-disable-next-line
+  const [isLoading, setIsLoading] = useState(true);
   const [isActive, setIsActive] = useState(false);
   // eslint-disable-next-line
   const dropdownRef = useRef(null);
+  const games = ["All Games", "Final Fantasy 01", "Final Fantasy BE", "Final Fantasy 02", "Final Fantasy 03", "Final Fantasy 04", "Final Fantasy 05", "Final Fantasy 06", "Final Fantasy 07", "Final Fantasy 08", "Final Fantasy 09", "Final Fantasy 10", "Final Fantasy 10-2", "Final Fantasy 12", "Final Fantasy 13", "Final Fantasy 13-2", "Final Fantasy 15"];
 
   useEffect(() => {
     const pageClickEvent = (e) => {
@@ -34,7 +37,8 @@ const Creatures = () => {
     if (playOnce) {
     axios
         .get('https://www.moogleapi.com/api/v1/monsters')
-        .then(res => {setData(res.data)})}
+        .then(res => {setData(res.data)})
+        .then(res => setIsLoading(false))}
   }, [data, playOnce]);
 
   return (
@@ -63,6 +67,7 @@ const Creatures = () => {
               </button>)})}</div>)
         :(null)}
     </div>
+    {isLoading && <LoadingSpinner />}
     <ul className="card-list">
       {(selectedGame === "All Games" || selectedGame === "")?
       data.map((crea) => (<CardC crea={crea} key="crea.monsterId" url={crea.picture} />))
