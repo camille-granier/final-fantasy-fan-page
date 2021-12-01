d0b11248969eab8d3a8eee09d53cddd4a1e8f6c3
camille-granier <granierc@protonmail.com>
2021-12-01 16:09:01 +0100

    add loading spinner on Games page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -3,18 +3,21 @@ import Navigation from '../components/Navigation';
 import axios from 'axios';
 import CardG from '../components/CardG';
 import Logo from '../components/Logo';
+import LoadingSpinner from '../UI/Loadingspinner';
 
 const Games = () => {
   const [data, setData] = useState([]);
   // eslint-disable-next-line
   const [playOnce, setPlayOnce] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   useEffect(() => {
     if (playOnce) {
     axios
         .get('https://www.moogleapi.com/api/v1/games')
-        .then(res => {setData(res.data)})}
+        .then(res => {setData(res.data)})
+        .then(res => setIsLoading(false))}
   }, [data, playOnce]);
 
   return (
@@ -24,6 +27,7 @@ const Games = () => {
       <Navigation />
     </div>
     <div className="sort-container"></div>
+    {isLoading && <LoadingSpinner />}
     <ul className="card-list">
       {data.map((game) => (<CardG game={game} key="game.gameId" url={game.picture} />))}
     </ul>
