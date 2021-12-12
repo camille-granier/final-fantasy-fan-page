e4cb2052b8210e38989ad2f00019471c74712b73
camille-granier <granierc@protonmail.com>
2021-12-12 17:38:23 +0100

    flipping card Creatures

--- src/UI/Loadingspinner.js
+++ src/UI/Loadingspinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-  <div class="lds-ripple"><div></div><div></div></div>
+  <div className="lds-ripple"><div></div><div></div></div>
 )};
 
 export default LoadingSpinner;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,35 @@
 import React from 'react';
-const Card = ({char, url}) => {
+const Card = ({char, url, des}) => {
+
+  let description = "";
+
+  if (des !== null) {
+     let arr = des.split('.', 3);
+     if (!arr[2]) {
+       description = description = arr[0] + "." + arr[1] + "."; 
+     } else {
+     description = arr[0] + "." + arr[1] + "." + arr[2] + ".";
+     }
+  }
 
   return (
-  <li className="card" key={char.index}>
-   <img src={url} alt="char-pic" />
-    <ul>
-      <li>{char.name}</li>
-      {(/[^?]/.test(char.age)? <li>Age: {char.age}</li>:"")}
-      {(/[^?]/.test(char.race)? <li>{char.race}</li>:"")}
-      <li>{char.origin}</li>
-    </ul>
-  </li>
+  <div className="flip-card">
+    <div className="flip-card-inner">
+      <li className="flip-card-front" key={char.index}>
+        <img src={url} alt="char-pic" />
+         <ul>
+           <li>{char.name}</li>
+            {(/[^?]/.test(char.age)? <li>Age: {char.age}</li>:"")}
+           <li>{char.origin}</li>
+        </ul>
+      </li>
+      <div className="flip-card-back">
+        {char.description === null 
+        ? <h5>{char.race} character from {char.origin}</h5> 
+        : <h6>{description}</h6>}
+      </div>
+    </div>
+  </div>
 )};
 
 export default Card;
