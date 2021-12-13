24a8168cec963e0ead93ea9bd65223c31da711d0
camille-granier <granierc@protonmail.com>
2021-12-13 10:27:58 +0100

    adding flip card Char and crea

--- src/components/CardC.js
+++ src/components/CardC.js
@@ -2,15 +2,35 @@ import React from 'react';
 
 const CardC = ({crea, url}) => {
 
+  let description = "";
+
+  if (crea.description !== null) {
+    let arr = crea.description.split(".", 2);
+
+    if (!crea.description[1]) {
+      description = arr[0] + ".";
+    } else {
+      description = arr[0] + "." + arr[1] + ".";
+    } 
+  }
 
   return (
-  <li className="card" key={crea.index}>
-  <img src={url} alt="creature-pic" />
-    <ul>
-      <li>{crea.name}</li>
-      <li>{crea.game}</li>
-    </ul>
-  </li>
+  <div className='flip-card'>
+    <div className='flip-card-inner'>
+    <li className="flip-card-front" key={crea.index}>
+      <img src={url} alt="creature-pic" />
+      <ul>
+       <li>{crea.name}</li>
+       <li>{crea.game}</li>
+     </ul>
+    </li>
+    <div className="flip-card-back">
+      {crea.description === null
+      ? <h5>{crea.name} from {crea.game}</h5>
+      : <h6>{description}</h6>}
+      </div>
+    </div>
+  </div>
 )};
 
 export default CardC;
