55c4a39e7ad22c179ed685df718895402d8db4a5
camille-granier <granierc@protonmail.com>
2022-01-26 12:54:17 +0100

    creating top dark line

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import Navigation from './Navigation';
 
 const Header = () => {
     return (
-        <div className="header">
-            <Logo />
-            <Navigation />
-        </div>
-    );
+        <div className='header-container'>
+            <div className='line'>Final Fantasy</div>
+                <div className="header">
+                    <Logo />
+                    <Navigation />
+                </div>
+                </div>
+                );
 };
 
-export default Header;
+                export default Header;
