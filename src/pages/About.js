7b3609ef1b50c14ba3e59533431cc94f8c9e760e
camille-granier <granierc@protonmail.com>
2022-02-01 10:34:46 +0100

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import Contact from '../components/Contact';
 
 const About = () => (
   <div>
     <Header />
   <div className="about">
-    <p>A react project by Camille Granier, using the wonderful
-      <a href="https://www.moogleapi.com/"
+    <p><span className='about-background'>A react project by Camille Granier, using the wonderful <a href="https://www.moogleapi.com/"
          target="_blank"
          rel='noreferrer'
          className="link"
-         > MoogleApi</a>
+         >MoogleApi</a></span>
     </p>
     </div>
   </div>
