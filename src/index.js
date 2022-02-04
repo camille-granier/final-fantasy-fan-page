c442178ca396681a795b7aadbd0b4e0d3a1bbb2c
camille-granier <granierc@protonmail.com>
2022-02-04 12:58:15 +0100

    setting env file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './App';
+import dotenv from "dotenv";
 
+
+
+require('dotenv').config();
+console.log(process.env.REACT_APP_USER_ID);
 
 ReactDOM.render(
   <React.StrictMode>
