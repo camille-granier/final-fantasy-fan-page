5dbbd217e1dca56b9680abeb428d6ffa2901597d
camille-granier <granierc@protonmail.com>
2022-03-08 22:05:09 +1100

    adding link icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Logo from './Logo';
 import Navigation from './Navigation';
+import { FaGithubSquare } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <div className='header-container'>
-            <div className='line'>FINAL FANTASY</div>
+            <div className='line'>
+                <p>FINAL FANTASY</p>
+                <div className='icons-container'>
+                        <a className="icon"
+                           target="_blank" 
+                           rel="noopener noreferrer"
+                           href="https://github.com/camille-granier"><FaGithubSquare /></a>
+                    </div>    
+             </div>
             <div className="header">
                 <Logo />
                 <input id="menu-toggle" type="checkbox" name="menu-toggle" />
