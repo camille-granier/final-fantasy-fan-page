6bafb95f5fd8b780a7e39a45a21483ad3e0fe2a8
camille-granier <granierc@protonmail.com>
2022-03-10 20:26:03 +1100

    correcting box-shadow navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,10 +3,10 @@ import {NavLink} from 'react-router-dom';
 
 const Navigation = () => (
   <div className="navigation">
-    <NavLink exact to="/" activeClassName="nav-active" >Characters</NavLink>
-    <NavLink exact to="/Games" activeClassName="nav-active">Games</NavLink>
-    <NavLink exact to="/Creatures" activeClassName="nav-active" >Creatures</NavLink>
-    <NavLink exact to="/About" activeClassName="nav-active" >About</NavLink>
+    <NavLink exact to="/" activeClassName="nav-active" >CHARACTERS</NavLink>
+    <NavLink exact to="/Games" activeClassName="nav-active">GAMES</NavLink>
+    <NavLink exact to="/Creatures" activeClassName="nav-active" >CREATURES</NavLink>
+    <NavLink exact to="/About" activeClassName="nav-active" >ABOUT</NavLink>
   </div>
 );
 
