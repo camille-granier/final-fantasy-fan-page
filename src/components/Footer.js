1c5321b4bf55d789f3edd58568305f061026acfc
camille-granier <granierc@protonmail.com>
2022-03-12 14:12:03 +1100

    modif footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
-            <p>© 2022 FFpage</p>
+            <p className='copyright'>© 2022 <a href="" className='title-link'>FFpage</a></p>
+            <a href="">Legal Notices</a>
+            <a href="" className='privacy-policy'>Privacy Policy</a>
+            <a href="">Contact</a>
         </div>
     );
 };
