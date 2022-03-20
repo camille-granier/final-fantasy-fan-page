f17602fa3180c103fac2431e78bc020eb0659d7c
camille-granier <granierc@protonmail.com>
2022-03-20 10:05:34 +0100

    adding scroll to top function to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, } from 'react-router-dom';
 import ScrollToTop from '../UI/ScrollToTop';
 
 const Footer = () => {
+
     return (
         <div className='footer'>
             <ScrollToTop />
-            <p className='copyright'>© 2022 <NavLink to="/" className='title-link'>Final Fantasy Notes</NavLink></p>
-            <NavLink to="/LegalNotices">Legal Notices</NavLink>
-            <NavLink to="/PrivacyPolicy" className='privacy-policy-link'>Privacy Policy</NavLink>
-            <NavLink to="/About">Contact</NavLink>
+            <p className='copyright'>© 2022  
+                <NavLink 
+                    to="/" 
+                    onClick={() => window.scrollTo(0, 0)}
+                    className='title-link'> <span>Final Fantasy Notes</span>
+                </NavLink>
+            </p>
+            <NavLink 
+                to="/LegalNotices"
+                onClick={() => window.scrollTo(0, 0)}>Legal Notices
+            </NavLink>
+            <NavLink 
+                to="/PrivacyPolicy" 
+                onClick={() => window.scrollTo(0, 0)} 
+                className='privacy-policy-link'>Privacy Policy
+            </NavLink>
+            <NavLink 
+                to="/About"
+                onClick={() => window.scrollTo(0, 0)}>Contact
+            </NavLink>
         </div>
     );
 };
