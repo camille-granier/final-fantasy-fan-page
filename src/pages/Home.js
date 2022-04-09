de20444f73403d93c01e15c9e1e8a46645c3e7fd
camille-granier <granierc@protonmail.com>
2022-04-09 10:27:54 +1000

    helmet for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
 import Characters from '../components/Characters';
+import { Helmet } from 'react-helmet';
 
 
 const Home = () => (
   <div className="home">
+    <Helmet>
+      <html lang="en" />
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Discover Final Fantasy Characters"/>
+      <title>Characters</title>
+      </Helmet>
     <Header />
     <Characters />
   </div>
